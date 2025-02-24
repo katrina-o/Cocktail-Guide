287b3fcf789f80ca6a7d860900ead737c46ca8f1
katrina-o <ekaterinaorlova25041994@gmail.com>
2025-02-24 13:51:49 +0300

    Initial Commit

---
+++ Cocktail Guide/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Cocktail Guide
+//
+//  Created by Екатерина Орлова on 24.02.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
