5c634bc6ad9dfc2664aa050d13816e2cf1151ccf
katrina-o <ekaterinaorlova25041994@gmail.com>
2025-02-26 14:16:33 +0300

    feat: add exstension

---
+++ Cocktail Guide/Extension/UILabel+Ex.swift
@@ -0,0 +1,26 @@
+//
+//  Extensions.swift
+//  Cocktail Guide
+//
+//  Created by Екатерина Орлова on 24.02.2025.
+//
+
+
+import UIKit
+
+extension UILabel {
+    static func createLabel(text: String,
+                            fontSize: CGFloat,
+                            isBold: Bool = false,
+                            textAlignment: NSTextAlignment = .center,
+                            numberOfLines: Int = 0) -> UILabel {
+        let label = UILabel()
+        label.text = text
+        label.textColor = .black
+        label.font = isBold ? UIFont.boldSystemFont(ofSize: fontSize) : UIFont.systemFont(ofSize: fontSize)
+        label.textAlignment = textAlignment
+        label.numberOfLines = numberOfLines
+        label.translatesAutoresizingMaskIntoConstraints = false
+        return label
+    }
+}
